779421914fcf094fe21ec6480e475d22d0f6f71e
eric373 <25942990+eric373@users.noreply.github.com>
2022-02-03 20:40:00 -0500

    Query and write all tables to parquet files

--- learn-spark/src/main/scala/Jdbc.scala
+++ learn-spark/src/main/scala/Jdbc.scala
@@ -10,13 +10,32 @@ object Main {
     println("  Jdbc")
     println("----------------------------------------------------------------------")
     val spark = SparkSession.builder().appName("Dataset Demo").getOrCreate() // Create SparkSession entry point.
+    
+    val tableQuery = """select s.name + '.' + t.name TableName
+      |from sys.tables t
+      |join sys.schemas s
+      |on t.schema_id = s.schema_id
+      |where s.name != 'dbo'""".stripMargin
+    val dftn = spark.read.format("jdbc")
+        .option("url",  "jdbc:sqlserver://localhost:1433;databaseName=AdventureWorks;trustServerCertificate=true;integratedsecurity=true")
+        .option("query", tableQuery)
+        .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver")
+        .load()
 
-    val df = (spark.read.format("jdbc")
-    .option("url",  "jdbc:sqlserver://localhost:1433;databaseName=AdventureWorks;trustServerCertificate=true;integratedsecurity=true")
-    .option("dbtable", "Person.Person")
-    .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver")
-    .load())
-    println(df.count())
+    // collect() returns Array[Row]
+    // tableNames: Array[String]
+    val tableNames = dftn.collect().map(_.getString(0))
+
+    tableNames.map((tableName) => {
+      val df = (spark.read.format("jdbc")
+        .option("url",  "jdbc:sqlserver://localhost:1433;databaseName=AdventureWorks;trustServerCertificate=true;integratedsecurity=true")
+        .option("dbtable", tableName)
+        .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver")
+        .load())
+      df.write.format("parquet").mode("overwrite").save(s".\\parquet\\AdventureWorks-oltp\\${tableName}.parquet")
+      // Another way to write parquet files:
+      // df.write.mode("overwrite").parquet(s".\\parquet\\AdventureWorks-oltp\\${tableName}.parquet")
+    })
 
     spark.stop()
     println()
