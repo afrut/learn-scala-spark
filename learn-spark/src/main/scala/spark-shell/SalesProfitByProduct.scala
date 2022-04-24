68edf810b8ebe34f963a2b9ce7ad5b077965097e
eric373 <25942990+eric373@users.noreply.github.com>
2022-04-24 11:48:19 -0400

    Added SalesPerson column

--- learn-spark/src/main/scala/spark-shell/SalesProfitByProduct.scala
+++ learn-spark/src/main/scala/spark-shell/SalesProfitByProduct.scala
@@ -5,38 +5,78 @@ import scala.util.control.Breaks._
 val product = spark.read.format("parquet").
   load(".\\parquet\\AdventureWorks-oltp\\Production.Product.parquet").
   withColumnRenamed("Name", "ProductName")
+val soh = spark.read.format("parquet").
+  load(".\\parquet\\AdventureWorks-oltp\\Sales.SalesOrderHeader.parquet")
 val sod = spark.read.format("parquet").
   load(".\\parquet\\AdventureWorks-oltp\\Sales.SalesOrderDetail.parquet")
+val person = spark.read.format("parquet").
+  load(".\\parquet\\AdventureWorks-oltp\\Person.Person.parquet")
 product.createOrReplaceTempView("product")
 sod.createOrReplaceTempView("sod")
+soh.createOrReplaceTempView("soh")
+person.createOrReplaceTempView("person")
 
-// Aggregate data based on product type
-val sodGrouped = sod.select("ProductID", "OrderQty", "LineTotal").
-  groupBy("ProductID").
+// Retrieve SalesPersonName
+val sp = soh.
+  join(person, soh("SalesPersonID") === person("BusinessEntityID"), "left").
+  withColumn("SalesPersonName", when(soh("SalesPersonID").isNull, "ONLINE")
+    .otherwise(concat(person("LastName"), lit(", "), person("FirstName")))).
+  select("SalesPersonName", "SalesOrderID")
+
+// Aggregate data based on sales person and product
+val sodGrouped = sod.
+  join(sp, sod("SalesOrderID") === sp("SalesOrderID"), "inner").
+  groupBy(sp("SalesPersonName"), sod("ProductID")).
   agg(sum("OrderQty").as("OrderQty")
-    , sum("LineTotal").as("LineTotal"))
+    , sum("LineTotal").as("LineTotal")).
+  select("SalesPersonName", "ProductID", "OrderQty", "LineTotal")
+
+// Compute profit
 val profit1 = product.
-  join(sodGrouped, product("ProductID") === sod("ProductID"), "inner").
+  join(sodGrouped, product("ProductID") === sodGrouped("ProductID"), "inner").
   withColumn("Profit", $"LineTotal" - ($"StandardCost" * $"OrderQty")).
-  select("ProductName", "Profit", "LineTotal", "StandardCost", "OrderQty")
+  select("SalesPersonName", "ProductName", "Profit", "LineTotal", "StandardCost", "OrderQty")
 profit1.show()
 
-// Same thing but with SQL
-val query = 
-  """select p.ProductName
-  |     , sod.LineTotal - (p.StandardCost * sod.OrderQty) Profit
-  |     , sod.LineTotal LineTotal
-  |     , p.StandardCost
-  |     , sod.OrderQty
-  |from product p
-  |join
-  |(
-  |  select ProductID
+// --------------------------------------------------------------------------------
+// Retrieve SalesPersonName
+spark.sql(
+  """select soh.SalesOrderID SalesOrderID
+  |       , case when p.BusinessEntityID is null then 'ONLINE'
+  |           else concat(p.LastName, ', ', p.FirstName)
+  |         end SalesPersonName
+  |from soh
+  |left join person p
+  |on soh.SalesPersonID = p.BusinessEntityID
+  """.stripMargin).
+  createOrReplaceTempView("sp")
+
+// Aggregate data based on sales person and product
+spark.sql(
+  """select sp.SalesPersonName SalesPersonName
+  |       , sod.ProductID ProductID
   |       , sum(LineTotal) LineTotal
   |       , sum(OrderQty) OrderQty
-  |  from sod
-  |  group by ProductID
-  |) sod
-  |on p.ProductID = sod.ProductID""".stripMargin
-val profit2 = spark.sql(query)
-profit2.show()
+  |from sod
+  |join sp
+  |on sod.SalesOrderID = sp.SalesOrderID
+  |group by sp.SalesPersonName
+  |       , sod.ProductID
+  """.stripMargin).
+  createOrReplaceTempView("sodGrouped")
+
+// Compute profit
+spark.sql(
+  """select sg.SalesPersonName SalesPersonName
+  |       , p.ProductName ProductName
+  |       , sg.LineTotal - (p.StandardCost * sg.OrderQty) Profit
+  |       , sg.LineTotal LineTotal
+  |       , p.StandardCost StandardCost
+  |       , sg.OrderQty OrderQty
+  |from sodGrouped sg
+  |join product p
+  |on sg.ProductID = p.ProductID
+  """.stripMargin).
+  createOrReplaceTempView("profit")
+
+spark.sql("select * from profit").show()
