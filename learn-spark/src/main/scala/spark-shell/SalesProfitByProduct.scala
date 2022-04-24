126cd1a17b002edaf2a2dcfebbe4a68ca99619fa
eric373 <25942990+eric373@users.noreply.github.com>
2022-04-24 09:18:09 -0400

    Added query that does the same thing

--- learn-spark/src/main/scala/spark-shell/SalesProfitByProduct.scala
+++ learn-spark/src/main/scala/spark-shell/SalesProfitByProduct.scala
@@ -1,4 +1,5 @@
 // Compute net profit of each product across all sales
+import scala.util.control.Breaks._
 
 // Load data
 val product = spark.read.format("parquet").
@@ -6,14 +7,36 @@ val product = spark.read.format("parquet").
   withColumnRenamed("Name", "ProductName")
 val sod = spark.read.format("parquet").
   load(".\\parquet\\AdventureWorks-oltp\\Sales.SalesOrderDetail.parquet")
+product.createOrReplaceTempView("product")
+sod.createOrReplaceTempView("sod")
 
 // Aggregate data based on product type
 val sodGrouped = sod.select("ProductID", "OrderQty", "LineTotal").
   groupBy("ProductID").
   agg(sum("OrderQty").as("OrderQty")
     , sum("LineTotal").as("LineTotal"))
-val profit = product.
+val profit1 = product.
   join(sodGrouped, product("ProductID") === sod("ProductID"), "inner").
   withColumn("Profit", $"LineTotal" - ($"StandardCost" * $"OrderQty")).
   select("ProductName", "Profit", "LineTotal", "StandardCost", "OrderQty")
-profit.show()
+profit1.show()
+
+// Same thing but with SQL
+val query = 
+  """select p.ProductName
+  |     , sod.LineTotal - (p.StandardCost * sod.OrderQty) Profit
+  |     , sod.LineTotal LineTotal
+  |     , p.StandardCost
+  |     , sod.OrderQty
+  |from product p
+  |join
+  |(
+  |  select ProductID
+  |       , sum(LineTotal) LineTotal
+  |       , sum(OrderQty) OrderQty
+  |  from sod
+  |  group by ProductID
+  |) sod
+  |on p.ProductID = sod.ProductID""".stripMargin
+val profit2 = spark.sql(query)
+profit2.show()
